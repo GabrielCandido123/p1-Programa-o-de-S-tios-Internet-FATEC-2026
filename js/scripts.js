7d0bcc6ce2c36bf2f8cf8bf29bd46dae3f1c274f
GabrielCandido123 <gabrielcandidodasilva25@gmail.com>
2026-04-16 23:35:57 -0300

    adicionando atividade 02 - consumir api ao repositório

---
+++ js/scripts.js
@@ -0,0 +1,50 @@
+"use strict";
+
+function criarTemplatePersonagem(item) {
+  const card = document.createElement("div");
+  card.classList.add("card-personagem");
+
+  const img = document.createElement("img");
+  img.src = item.character.images.jpg.image_url;
+  img.alt = item.character.name;
+
+  const p = document.createElement("p");
+  p.classList.add("descricao-personagem");
+  p.textContent = `${item.character.name} - ${item.role}`;
+
+  card.appendChild(img);
+  card.appendChild(p);
+
+  return card;
+}
+
+async function buscarPersonagens() {
+  const nomeAnime = document.getElementById("digitar-anime").value;
+  const galeria = document.getElementById("galeria-personagens");
+
+  if (nomeAnime === "") {
+    return;
+  } else {
+    const respostaAnime = await fetch(
+      `https://api.jikan.moe/v4/anime?q=${nomeAnime}&limit=1`,
+    );
+    const dadosAnime = await respostaAnime.json();
+    const animeId = dadosAnime.data[0].mal_id;
+
+    const respostaPersonagens = await fetch(
+      `https://api.jikan.moe/v4/anime/${animeId}/characters`,
+    );
+    const dadosPersonagens = await respostaPersonagens.json();
+
+    galeria.replaceChildren();
+
+    dadosPersonagens.data.forEach((item) => {
+      const cardPronto = criarTemplatePersonagem(item);
+      galeria.appendChild(cardPronto);
+    });
+  }
+}
+
+document
+  .getElementById("buscar-personagens")
+  .addEventListener("click", buscarPersonagens);
